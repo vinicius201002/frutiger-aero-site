762bb2301ae12b436fb5cd6594c8e4c5c0e9eec6
vinicius201002 <vinicius.opedroso@sptech.school>
2025-03-27 12:49:34 -0300

    fix: Corrige configuração de CORS

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url'; // Import necessário para __dirname
+import { fileURLToPath } from 'url';
 import config from './config/env.js';
 import connectDB from './config/db.js';
 import messageRoutes from './routes/messageRoute.js';
+import cors from 'cors';
 
-// Configuração do __dirname para módulos ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', true);
 const port = 3000;
 
+// Configuração CORRETA do CORS (deve vir antes das rotas)
+app.use(cors({
+  origin: 'https://frutigeraero.duckdns.org', // Permite apenas este domínio
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  credentials: true // Se estiver usando cookies/sessão
+}));
+
 // Middlewares
-app.use(express.json()); // Para parsear JSON
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Rotas
@@ -21,8 +29,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-
-// Roteamento de criação de mensagens
 app.use('/api/messages', messageRoutes);
 
 // 404
@@ -30,9 +36,8 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, '../public', '404.html'));
 });
 
-
 // Inicia servidor
-app.listen(port, async () => {
+app.listen(port, '0.0.0.0', async () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
   await connectDB(); 
   console.log(`Conectando ao MongoDB: ${config.MONGO_URI}`);
